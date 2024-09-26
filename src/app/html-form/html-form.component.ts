4bf26dcd3fdc7ee5cb07a42bb244fb21f443d799
Jackweb-rgba <radohery09@gmail.com>
2024-09-26 08:51:14 +0200

    utilisation validation et valueChanges

--- src/app/html-form/html-form.component.ts
+++ src/app/html-form/html-form.component.ts
@@ -1,25 +1,58 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { log } from 'console';
+import { NgClass, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { log } from 'node:console';
 
 @Component({
   selector: 'app-html-form',
   standalone: true,
   imports: [
       ReactiveFormsModule,
+      NgClass,
+      NgIf
   ],
   templateUrl: './html-form.component.html',
   styleUrl: './html-form.component.scss'
 })
-export class HtmlFormComponent {
 
-  formGroup = new FormGroup({
-    firstname: new FormControl(''),
-    lastname: new FormControl(''),
-    age: new FormControl(0)
-  })
+export class HtmlFormComponent implements OnInit{
+  formGroup!: FormGroup;
+  birthDay !: Number
+
+  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      firstname: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      lastname: new FormControl(''),
+      age: new FormControl(0,[Validators.required,Validators.min(10),Validators.max(90)])
+    })
+
+    this.birthDay = 2024;
+    this.formGroup.controls['age'].valueChanges.subscribe((currentAgeValue) =>{
+      this.birthDay =  2024 - this.formGroup.controls['age'].value
+    })
+  }
+
+
+
 
   onSubmit() {
-    console.log(this.formGroup.value);
+    this.formGroup.markAllAsTouched();
+    if (this.formGroup.invalid){
+      return;
+    }
+    console.warn(this.formGroup.value);
   }
+
+  getFirstname() : string {
+    return this.formGroup.controls['firstname'].value;
+  }
+  getLastname() : string {
+    return this.formGroup.controls['lastname'].value;
+  }
+  getAge() : number {
+    return this.formGroup.controls['age'].value;
+  }
+
+
+
 }
